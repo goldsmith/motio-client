9f031acf43ca4b3e62ac47a11f617723272b2aef
Jonathan Goldsmith <jhghank@gmail.com>
2013-11-09 02:23:42 -0500

    even more stuuuuf

--- client.py
+++ client.py
@@ -5,18 +5,19 @@ from flask_sockets import Sockets
 
 from websocket import create_connection
 
-from client import pipe
+from utils import pipe
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
 gestures = {}
+web_socket = None
 
 def run(name):
 	path = gestures[name]
 	# do something with path
 
-@app.route("/"):
+@app.route("/")
 def index():
 	return render_template('page.html')
 
@@ -28,22 +29,33 @@ def add_gesture_filepath():
 	pass
 
 def get_from_server():
+	# ws = create_connection("ws://localhost:8000/client_socket")
 	ws = create_connection("ws://motio.herokuapp.com/client_socket")
+	print ws
 	while True:
+		print "hi", ws
 		data = ws.recv()
+		print 'data', data
 		if data["action"] == "add_gesture":
-
+			gestures[data["name"]] = ''
+			web_socket.send(data)
+		elif data["action"] == "do_gesture":
+			run(gestures[data["name"]])
+		else:
+			print "damn!"
+ 
 web_view = pipe()
 
 @sockets.route('/gesture')
 def echo_socket(ws):
-    for message in web_view:
-    	if message['action'] == "do_gesture":
-    		run(message['name'])
-    	else:
-        	ws.send(message)
+
+	web_socket = ws
+
+	while True:
+		continue
 
 if __name__ == '__main__':
-	# start_gui()
+	server = multiprocessing.Process(target=get_from_server)
+	server.start()
 	# connect_to_server()
-    app.run()
+	app.run()
