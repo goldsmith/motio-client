299659ea5136d9550e0cd5b11d93c987eb86b64c
Nick Kaashoek <nick.kaasheok@gmail.com>
2013-11-08 21:05:56 -0500

    X

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import Tkinter
 from flask import Flask
 from flask_sockets import Sockets
 from Tkinter import *
@@ -7,27 +8,36 @@ sockets = Sockets(app)
 
 gestures = {}
 
-@sockets.route('/get_all_gestures')
-def set_gestures(ws):
-	while True:
-		names = ws.receive()
-		for name in names:
-			gestures[name] = ''
+# @sockets.route('/get_all_gestures')
+# def set_gestures(ws):
+# 	while True:
+# 		names = ws.receive()
+# 		for name in names:
+# 			gestures[name] = ''
 
-@sockets.route('/add_gesture')
-def add_gesture(ws):
-	while True:
-		name = ws.receive()
-		gestures[name] = ''
+# @sockets.route('/add_gesture')
+# def add_gesture(ws):
+# 	while True:
+# 		name = ws.receive()
+# 		gestures[name] = ''
 
-@sockets.route('/do_gesture')
-def do_gesture(ws):
-    while True:
-        name = ws.receive()
-        program = gestures[name]
-        run(program)
+# @sockets.route('/do_gesture')
+# def do_gesture(ws):
+#     while True:
+#         name = ws.receive()
+#         program = gestures[name]
+#         run(program)
+
+def start_gui():
+	master = Tk()
+	listbox = Listbox(master)
+	listbox.pack()   
+	listbox.insert(END, "a list entry")
+	for item in ["one", "two", "three", "four"]:
+   	 	listbox.insert(END, item)
+	mainloop()
 
 if __name__ == '__main__':
 	start_gui()
-	connect_to_server()
-    app.run()
+	# connect_to_server()
+    # app.run()
