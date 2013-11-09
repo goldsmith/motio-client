c0da196f6e005f0f270f00a589fa79005e81d191
Jonathan Goldsmith <jhghank@gmail.com>
2013-11-08 20:17:03 -0500

    initial commit

---
+++ client.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/command')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        do_command(message)
+
+def do_command(command):
+	"""
+	Execute a command recieved from the server (should be a dictionary)
+
+	command = {
+		"command": <command>
+	}
+
+	<command> should be one of (so far):
+	(
+		"volume up",
+		"volume down",
+		"volume mute"
+	)
+
+	Hint: look into system commands
+
+	"""
+
+	pass
